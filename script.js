e39348a10110f5bd64f0629f5249924ba0f06505
Ritesh <riteshcph123@rediffmail.com>
2022-06-30 13:40:04 +0530

    beautified

--- script.js
+++ script.js
@@ -1,42 +1,41 @@
 let userScore = parseInt(0);
 let computerScore = parseInt(0);
 
-function computerPlay() {                                    
 
-    const randomNumber = Math.floor(Math.random()*3);            //Math.floor function rounds the number a nearest integer as Math.random returns a floating value. That's why!//
+function computerPlay() {
+
+    const randomNumber = Math.floor(Math.random() * 3); //Math.floor function rounds the number a nearest integer as Math.random returns a floating value. That's why!//
     switch (randomNumber) {
 
-        case 0: 
+        case 0:
             return 'rock';
 
-        case 1: 
+        case 1:
             return 'paper';
 
-        case 2: 
+        case 2:
             return 'siss';
-    };  
+    };
 
 };
 
-function singleRound (playerSelection, computerSelection) {
+function singleRound(playerSelection, computerSelection) {
 
-if ((playerSelection.toLowerCase() === 'rock' && computerSelection === 'siss') ||
-    (playerSelection.toLowerCase() === 'paper' && computerSelection === 'rock')||
-    (playerSelection.toLowerCase() === 'siss' && computerSelection === 'paper')) {
+    if ((playerSelection.toLowerCase() === 'rock' && computerSelection === 'siss') ||
+        (playerSelection.toLowerCase() === 'paper' && computerSelection === 'rock') ||
+        (playerSelection.toLowerCase() === 'siss' && computerSelection === 'paper')) {
         userScore++;
-        return 'Player Win';                                           // Statements after Return don't execute.
-    } else if((playerSelection.toLowerCase() === 'siss' && computerSelection === 'rock')||
-              (playerSelection.toLowerCase() ==='rock' && computerSelection === 'paper')||
-              (playerSelection.toLowerCase() === 'paper' && computerSelection === 'siss')){
+        return 'Player Win'; // Statements after Return don't execute.
+    } else if ((playerSelection.toLowerCase() === 'siss' && computerSelection === 'rock') ||
+        (playerSelection.toLowerCase() === 'rock' && computerSelection === 'paper') ||
+        (playerSelection.toLowerCase() === 'paper' && computerSelection === 'siss')) {
 
-        computerScore++;        
-        return 'Computer Win';                                          // Statements after Return don't execute.
-        
+        computerScore++;
+        return 'Computer Win'; // Statements after Return don't execute.
+
     } else if (playerSelection.toLowerCase() === computerSelection) {
         return 'It\'s a tie';
-    }
-    
-    else {
+    } else {
         return 'typo mistake';
     }
 
@@ -44,17 +43,15 @@ if ((playerSelection.toLowerCase() === 'rock' && computerSelection === 'siss') |
 
 
 function myGame() {
-    for (var i = 0; i<5; i++) {
+    for (var i = 0; i < 5; i++) {
         const playerSelection = prompt("Enter either of Rock, Paper or Siss:")
         const computerSelection = computerPlay();
         console.log(`Player selection is ${playerSelection} and Player Score is ${userScore}`);
         console.log(`Computer selection is ${computerSelection} and Computer Score is ${computerScore}`);
         console.log(singleRound(playerSelection, computerSelection));
-    
+
     }
-    
+
 }
 
- console.log(myGame());
-
-
+console.log(myGame());
